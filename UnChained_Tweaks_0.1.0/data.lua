20708a873c00117b5a479144dc6764aaa52a1ff0
Nikolai Shamanovich <shm013@yandex.ru>
2017-03-29 19:30:47 +0300

    Revert "ut004"
    This reverts commit 5eaaec278b02acd67c3c2478a71f2fe947b4e9a2.

--- UnChained_Tweaks_0.1.0/data.lua
+++ UnChained_Tweaks_0.1.0/data.lua
@@ -1,3 +1,20 @@
+--{{ List of prototypes here: 
+entities = {
+	"storage"
+	}
+items = {
+	"storage"
+	}
+categories = {
+	"category"
+	}
+recipes = {
+	"recipe"
+	}
+technologies = {
+	"technology"
+	}
+--}}
 
 if not UT_Config then UT_Config = {} end
 if not UT_Config.mod then UT_Config.mod = {} end
@@ -6,9 +23,26 @@ if not UT_Functions then UT_Functions = {} end
 if not thxbob then thxbob = {} end
 if not thxbob.lib then thxbob.lib = {} end
 
-require("libs.recipe-functions")
-require("prototypes.UT-recipe")
-require("prototypes.UT-buildings")
-require("prototypes.UT-equipment-grid")
-require("prototypes.UT-technology")
-require("prototypes.UT-category")
+for k,v in pairs(entities) do
+	require("prototypes" .. ".entity." .. v)
+end
+
+for k,v in pairs(items) do
+	require("prototypes" .. ".item." .. v)
+end
+
+for k,v in pairs(recipes) do
+	require("prototypes" .. ".recipe." .. v)
+end
+
+for k,v in pairs(categories) do
+	require("prototypes" .. ".category." .. v)
+end
+
+for k,v in pairs(technologies) do
+	require("prototypes" .. ".technology." .. v)
+end
+
+--require("libs.recipe-functions")
+
+require("prototypes.equipment-grid")
--- UnChained_Tweaks_0.1.0/libs/recipe-functions.lua
+++ UnChained_Tweaks_0.1.0/libs/recipe-functions.lua
@@ -11,32 +11,6 @@ UTweaks.addtorecipe = function(recipe, item)
 end
 
 
-UTweaks.remove_ingredient = function(recipe, item)
-	for i, object in ipairs(data.raw.recipe[recipe].ingredients) do
-		if object[1] == item or object.name == item then
-			table.remove(data.raw.recipe[recipe].ingredients, i)
-		end
-	end	
-end
 
--- UTweaks.change_ingredient_count = function(recipe, item, Icount)
-	-- for i, object in ipairs(data.raw.recipe[recipe].ingredients) do
-		-- if object[1] == item or object.name == item then
-			-- table.remove(data.raw.recipe[recipe].ingredients, i)
-		-- end
-	-- end	
-	-- if type(item[1]) == "table" then
-		-- for index, o in ipairs(item) do
-			-- table.insert( data.raw["recipe"][recipe].ingredients, o)
-		-- end
-		-- else
-			-- table.insert( data.raw["recipe"][recipe].ingredients, {{item, Icount}})
-		-- end
--- end
-
-UTweaks.resort = function(Itype, Iname, Isubgroup, Iorder)
-	data.raw[Itype][Iname].subgroup = Isubgroup
-	data.raw[Itype][Iname].order = Iorder
-end	
 
 
